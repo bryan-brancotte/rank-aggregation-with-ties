14d52ba1f9098a621ccc81e14d32f4ef9213b40f
pierreandrieu <pi.andrieu.94@gmail.com>
2017-12-08 14:28:47 +0100

    add unification and projection class

---
+++ sources/rnt/mediane/normalizations/projection.py
@@ -0,0 +1,46 @@
+from typing import List
+
+from mediane.datasets.dataset import Dataset
+from mediane.normalizations.normalize import Normalization
+
+
+class Projection(Normalization):
+
+    def rankings_to_rankings(rankings: List[List[List[int]]]) -> List[List[List[int]]]:
+        elements = {}
+        projected_rankings = []
+        m = len(rankings)
+        for ranking in rankings:
+            for bucket in ranking:
+                for element in bucket:
+                    if element in elements:
+                        elements[element] += 1
+                    else:
+                        elements[element] = 1
+        for ranking in rankings:
+            new_ranking = []
+            projected_rankings.append(new_ranking)
+            for bucket in ranking:
+                new_bucket = []
+                for element in bucket:
+                    if elements[element] == m:
+                        new_bucket.append(element)
+                if len(new_bucket) > 0:
+                    new_ranking.append(new_bucket)
+
+        return projected_rankings
+
+    def rankings_to_dataset(rankings: List[List[List[int]]]) -> Dataset:
+        return Dataset(Projection.rankings_to_rankings(rankings))
+
+    def dataset_to_rankings(d: Dataset) -> List[List[List[int]]]:
+        return Projection.rankings_to_rankings(d.rankings)
+
+    def dataset_to_dataset(d: Dataset) -> Dataset:
+        return Projection.rankings_to_dataset(d.rankings)
+
+    rankings_to_rankings = staticmethod(rankings_to_rankings)
+    rankings_to_dataset = staticmethod(rankings_to_dataset)
+    dataset_to_rankings = staticmethod(dataset_to_rankings)
+    dataset_to_dataset = staticmethod(dataset_to_dataset)
+
--- sources/rnt/mediane/normalizations/unification.py
+++ sources/rnt/mediane/normalizations/unification.py
@@ -1,33 +1,52 @@
 from typing import List
 
 from mediane.normalizations.normalize import Normalization
+from mediane.datasets.dataset import Dataset
 
+class Unification(Normalization):
 
-class Projection(Normalization):
-
-    def rankings_to_normalized_rankings(rankings: List[List[List[int]]]) -> List[List[List[int]]]:
-        elements = {}
-        projected_rankings = []
-        m = len(rankings)
-        for ranking in rankings:
-            for bucket in ranking:
-                for element in bucket:
-                    if element in elements:
-                        elements[element] += 1
-                    else:
-                        elements[element] = 0
-
+    def rankings_to_rankings(rankings: List[List[List[int]]]) -> List[List[List[int]]]:
+        copy_rankings = []
         for ranking in rankings:
             new_ranking = []
-            projected_rankings.append(new_ranking)
+            copy_rankings.append(new_ranking)
             for bucket in ranking:
-                new_bucket = []
+                new_ranking.append(list(bucket))
+
+        elements = {}
+        dict_rankings = {}
+        id_ranking = 0
+        for ranking in copy_rankings:
+            dict_rankings[id_ranking] = {}
+            dict_of_elements = dict_rankings[id_ranking]
+            for bucket in ranking:
                 for element in bucket:
-                    if elements[element] == m:
-                        new_bucket.append(element)
-                if len(new_bucket) > 0:
-                    new_ranking.append(new_bucket)
+                    dict_of_elements[element] = 0
+                    if element not in elements:
+                        elements[element] = 0
+            id_ranking += 1
+        id_ranking = 0
+        for ranking in copy_rankings:
+            dict_of_elements = dict_rankings[id_ranking]
+            if len(dict_of_elements) != len(elements):
+                bucket_unif = []
+                for element in elements.keys():
+                    if element not in dict_of_elements:
+                        bucket_unif.append(element)
+                ranking.append(bucket_unif)
+            id_ranking += 1
+        return copy_rankings
 
-        return projected_rankings
+    def rankings_to_dataset(rankings: List[List[List[int]]]) -> Dataset:
+        return Dataset(Unification.rankings_to_rankings(rankings))
 
-    rankings_to_normalized_rankings = staticmethod(rankings_to_normalized_rankings)
+    def dataset_to_rankings(d: Dataset) -> List[List[List[int]]]:
+        return Unification.rankings_to_rankings(d.rankings)
+
+    def dataset_to_dataset(d: Dataset) -> Dataset:
+        return Unification.rankings_to_dataset(d.rankings)
+
+    rankings_to_rankings = staticmethod(rankings_to_rankings)
+    rankings_to_dataset = staticmethod(rankings_to_dataset)
+    dataset_to_rankings = staticmethod(dataset_to_rankings)
+    dataset_to_dataset = staticmethod(dataset_to_dataset)
