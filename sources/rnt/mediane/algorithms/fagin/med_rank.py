d560ca96191571cfb9cdbe0882d4dfe596b67ed9
pierreandrieu <pi.andrieu.94@gmail.com>
2017-12-21 11:29:27 +0100

    medrank is now usable with indiced kt and borda is decleared as usable with induced kt

--- sources/rnt/mediane/algorithms/fagin/med_rank.py
+++ sources/rnt/mediane/algorithms/fagin/med_rank.py
@@ -1,15 +1,19 @@
 from itertools import zip_longest
-from math import ceil
 from typing import List
 
 from mediane.algorithms.median_ranking import MedianRanking
-from mediane.distances.enumeration import GENERALIZED_KENDALL_TAU_DISTANCE
+from mediane.distances.enumeration import GENERALIZED_KENDALL_TAU_DISTANCE, GENERALIZED_INDUCED_KENDALL_TAU_DISTANCE
 
 
 class MedRank(MedianRanking):
     def __init__(self,  h=0.5):
+        if h < 0:
+            h = 0
+        elif h > 1:
+            h = 1
         self.h = h
 
+    # Complexity : 0 (2 * n) with adaptation for induced measure
     def compute_median_rankings(self, rankings: List[List[List[int]]], return_at_most_one_ranking: bool = False):
         """
         :param rankings: A set of rankings
@@ -20,14 +24,19 @@ class MedRank(MedianRanking):
         If the algorithm is not able to provide multiple consensus, or if return_at_most_one_ranking is True then, it
         should return a list made of the only / the first consensus found
         """
-        h = {}
+
+        has = {}
+
+        nb_rankings_needed = {}
         already_put = set()
-        m = len(rankings)
-        nb_rankings_needed = ceil(self.h * m)
-        if nb_rankings_needed < 1:
-            nb_rankings_needed = 1
-        if nb_rankings_needed > m:
-            nb_rankings_needed = m
+
+        for ranking in rankings:
+            for bucket in ranking:
+                for element in bucket:
+                    if element not in nb_rankings_needed:
+                        nb_rankings_needed[element] = self.h
+                    else:
+                        nb_rankings_needed[element] += self.h
 
         bucket_res = []
         ranking_res = []
@@ -37,14 +46,14 @@ class MedRank(MedianRanking):
                 if bucket is not None:
                     for element in bucket:
                         if element not in already_put:
-                            if element not in h:
-                                h[element] = 1
-                                if nb_rankings_needed == 1:
+                            if element not in has:
+                                has[element] = 1
+                                if nb_rankings_needed[element] <= 1:
                                     bucket_res.append(element)
                                     already_put.add(element)
                             else:
-                                h[element] += 1
-                                if h[element] == nb_rankings_needed:
+                                has[element] += 1
+                                if has[element] >= nb_rankings_needed[element]:
                                     bucket_res.append(element)
                                     already_put.add(element)
             if len(bucket_res) > 0:
@@ -68,5 +77,5 @@ class MedRank(MedianRanking):
         :return: a list of distances from distance_enumeration
         """
         return (
-            GENERALIZED_KENDALL_TAU_DISTANCE,
+            GENERALIZED_KENDALL_TAU_DISTANCE, GENERALIZED_INDUCED_KENDALL_TAU_DISTANCE
         )
