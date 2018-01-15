e7dac159b552a0dd5b591d7ae7c3ef066ed529d1
pierreandrieu <pi.andrieu.04@gmail.com>
2018-01-15 16:11:38 +0100

    fix coefficents for scoring computation

--- sources/rnt/mediane/algorithms/ailon/kwiksort/kwiksort_random.py
+++ sources/rnt/mediane/algorithms/ailon/kwiksort/kwiksort_random.py
@@ -51,10 +51,10 @@ class KwikSortRandom(KwikSortAbs):
         d = count_nonzero(element_var == -1)
         e = count_nonzero(element_var < pivot_var)
 
-        comp = [e-d+c, m-e-2*c-b+2*a, b-a, c-a, d-a, a]
+        comp = [e-d+a, m-e-b-c+a, b-a, c-a, d-a, a]
         cost_before = vdot(coeffs_distance[0], comp)
         cost_same = vdot(coeffs_distance[1], comp)
-        cost_after = vdot(coeffs_distance[0], [m-e-2*c-b+2*a,  e-d+c, b-a, d-a, c-a, a])
+        cost_after = vdot(coeffs_distance[0], [m-e-b-c+a,  e-d+a, b-a, d-a, c-a, a])
 
         if cost_same <= cost_before:
             if cost_same <= cost_after:
