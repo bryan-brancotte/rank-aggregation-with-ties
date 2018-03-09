bfd6aca4764a633866e3b68c0cb6196d590edd35
pierreandrieu <pi.andrieu.94@gmail.com>
2018-03-09 17:10:29 +0100

    modifications for production. Change debug false, change and hide secret key add path to cplex

--- sources/rnt/rnt/wsgi.py
+++ sources/rnt/rnt/wsgi.py
@@ -11,6 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+basedir = os.path.dirname(os.path.dirname(__file__))
+import sys
+sys.path.insert(0, basedir)
+sys.path.insert(0,'/opt/ibm/ILOG/CPLEX_Studio128/cplex/python/3.5/x86-64_linux')
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rnt.settings")
 
 application = get_wsgi_application()
