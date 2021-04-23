db8ad6d694777b5464acd4a1448f152b5bccfee1
andrieu <pierre.andrieu@lri.fr>
2021-04-23 21:45:24 +0200

    allow empty rankings

--- sources/rnt/mediane/distances/KendallTauGeneralizedNlogN.py
+++ sources/rnt/mediane/distances/KendallTauGeneralizedNlogN.py
@@ -1,16 +1,14 @@
 from typing import Dict, List
 from mediane.distances.distance_calculator import DistanceCalculator
 from mediane.distances.kemeny_computation import KemenyComputingFactory
-from mediane.distances.ScoringScheme import ScoringScheme
 
 from numpy import vdot, asarray
 
 
 class KendallTauGeneralizedNlogN(DistanceCalculator):
 
-    def __init__(self, distance: 'mediane.models.Distance', sc=ScoringScheme.get_scoring_scheme_when_no_distance()):
+    def __init__(self, distance: 'mediane.models.Distance'):
         self.distance = distance
-        self.sc = sc
 
     def get_distance_to_an_other_ranking(
             self,
@@ -33,12 +31,9 @@ class KendallTauGeneralizedNlogN(DistanceCalculator):
         (vect_before, vect_tied) = KemenyComputingFactory.get_before_tied_counting(elements_r1, size_buckets, ranking,
                                                                                    id_max)
         res = {}
-        id_order = 0
-        if self.distance is None:
-            coeffs = self.sc.matrix
-        else:
-            coeffs = asarray(self.distance.scoring_scheme)
-            id_order = self.distance.id_order
+
+        coeffs = asarray(self.distance.scoring_scheme)
+        id_order = self.distance.id_order
 
         dst = abs(vdot(coeffs[0], vect_before)) + abs(vdot(coeffs[1], vect_tied))
 
